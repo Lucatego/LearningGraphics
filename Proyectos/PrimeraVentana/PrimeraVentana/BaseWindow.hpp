a849c26561bf2dc6051ef3e74ab55af77510217a
Lucatego <lucasalvites@gmail.com>
2025-01-11 23:34:21 -0500

    End Modulo 1

--- Proyectos/PrimeraVentana/PrimeraVentana/BaseWindow.hpp
+++ Proyectos/PrimeraVentana/PrimeraVentana/BaseWindow.hpp
@@ -9,7 +9,7 @@ public:
 
         if (uMsg == WM_NCCREATE){
             CREATESTRUCT * pCreate = (CREATESTRUCT *)lParam;
-            pThis = (DERIVED_TYPE*)pCreate->lpCreateParams;
+            pThis = (DERIVED_TYPE *)pCreate->lpCreateParams;
             SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
 
             pThis->m_hwnd = hwnd;
@@ -25,12 +25,12 @@ public:
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
         }
     }
+    //Constructor para inicializar ese parametro en NULL
+    BaseWindow() : m_hwnd(NULL) {/*Acá el parámetro ya está inicilaizado*/}
 
-    BaseWindow() : m_hwnd(NULL) {}
-
-    BOOL Create(PCWSTR lpWindowName, DWORD dwStyle, DWORD dwExStyle = 0,
-            int x = CW_USEDEFAULT, int y = CW_USEDEFAULT, int nWidth = CW_USEDEFAULT, int nHeight = CW_USEDEFAULT,
-            HWND hWndParent = 0, HMENU hMenu = 0){
+    //Acá se crea la ventana
+    BOOL Create(PCWSTR lpWindowName, DWORD dwStyle, DWORD dwExStyle = 0, int x = CW_USEDEFAULT, int y = CW_USEDEFAULT, 
+            int nWidth = CW_USEDEFAULT, int nHeight = CW_USEDEFAULT, HWND hWndParent = 0, HMENU hMenu = 0){
 
         WNDCLASS wc = { 0 };
 
@@ -39,21 +39,21 @@ public:
         wc.lpszClassName = ClassName();
 
         RegisterClass(&wc);
-
+        //Creamos la ventana y la guardamos en el atributo de HWND
         m_hwnd = CreateWindowEx(
             dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
             nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this
         );
-
+        //Por ahora no hay loop
         return (m_hwnd ? TRUE : FALSE);
     }
 
     HWND Window() const { return m_hwnd; }
 
 protected:
-
+    //Lo vuelve abstracta
     virtual PCWSTR  ClassName() const = 0;
     virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
-
+    //Lo que se inicializa
     HWND m_hwnd;
 };
--- Proyectos/PrimeraVentana/PrimeraVentana/MainWindow.hpp
+++ Proyectos/PrimeraVentana/PrimeraVentana/MainWindow.hpp
@@ -1,5 +1,55 @@
 #include "BaseWindow.hpp"
 
+//Una clase hija
 class MainWindow :public BaseWindow<MainWindow> {
+public:
+    //Se define el nombre de la clase
+	PCWSTR ClassName() const {
+		return L"Ventana de ejemplo";
+	}
 
+	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam){
+        switch (uMsg){
+            //Cerar la ventana
+            case WM_DESTROY:
+                PostQuitMessage(0);
+                return 0;
+            //Pintar
+            case WM_PAINT: {
+                PAINTSTRUCT ps;
+                HDC hdc = BeginPaint(m_hwnd, &ps);
+                FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+                EndPaint(m_hwnd, &ps);
+            }
+                return 0;
+            //Ninguna de las anteriores
+            default:
+                return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+        }
+        return TRUE;
+    }
 };
+
+/*
+//Como se verian los metodos usando clases
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow){
+    //Una instancia de MainWindow
+    MainWindow win;
+    //Se crea usando el método de la clase abstracta
+    if (!win.Create(L"Learn to Program Windows", WS_OVERLAPPEDWINDOW)){
+        return 0;
+    }
+    //Se muestra la ventana
+    ShowWindow(win.Window(), nCmdShow);
+
+    // Run the message loop.
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0)){
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+*/
